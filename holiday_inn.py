0ca4fafe486eabd132485dede8cfdbee0207af93
Patrick <pateast678@gmail.com>
2019-11-18 09:40:48 -0500

    Changed interface, added linebreaks and underlines

--- holiday_inn.py
+++ holiday_inn.py
@@ -9,18 +9,27 @@ hotel = {
     '333': ['Neo', 'Trinity', 'sMorpheus']
   }
 }
+def add_guest(list, number):
+    count = 0
+    while count > number:
+        list.append('Guest name: ')
+    return list
+
 
 # check in function
-# def check_in(floor_number, room_number):
-#     if hotel[floor_number][room_number]:
-#         print('This room is occupied. Please choose another room\n')
-#         new_floor = input('New floor number: ')
-#         new_room = input('New room number: ')
-#         check_in(new_floor, new_room)
-#     else:
-#         number_of_guests  = int(input('Number of guests: '))
-#         guests = input('Guest: ') * number_of_guests
-    # hotel[floor_number][room_number] = 'test'
+def check_in(floor_number, room_number):
+    if floor_number in hotel:
+        if room_number in hotel[floor_number]:
+            print('This room is occupied. Please choose another room\n')
+            new_floor = input('New floor number: ')
+            new_room = input('New room number: ')
+            check_in(new_floor, new_room)
+        guest_list = []
+        number_of_guests = int(input('How many guests? '))
+        add_guest(guest_list, number_of_guests)
+        print(guest_list)
+        # hotel[floor_number][room_number] = guests
+
 # checking out-remove the occupants
 def check_out(floor_number, room_number):
     pass
